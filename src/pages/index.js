07afa43414ad3e517be7334665c3ecbd630cf57e
Kris Urbas <krzysu@interia.pl>
2018-03-30 10:28:08 +0700

    restructure index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,50 @@
 import React from 'react'
-import IndexPage from 'components/IndexPage/IndexPage'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import get from 'lodash/get'
+import BEMHelper from 'react-bem-helper'
+import Img from 'gatsby-image'
+import { getPostItemFlatData } from 'helpers'
+import PostList from 'components/PostList/PostList'
+import AuthorItem from 'components/AuthorItem/AuthorItem'
+import './index.scss'
 
-const BlogIndex = ({ data }) => {
-  return <IndexPage data={data} />
+const bem = new BEMHelper('index-page')
+
+const IndexPage = ({ data }) => {
+  const siteTitle = get(data, 'site.siteMetadata.title')
+  const posts = get(data, 'allMarkdownRemark.edges', [])
+  const coverImageSizes = get(data, 'coverImage.sizes')
+
+  const flatPosts = posts.map(getPostItemFlatData)
+
+  return (
+    <div {...bem()}>
+      <Helmet title={siteTitle} />
+      {coverImageSizes && (
+        <Img sizes={coverImageSizes} {...bem('cover-image')} />
+      )}
+      <div className="wrapper wrapper--wide">
+        <div className="page__header">
+          <h2 className="page__title">Blog</h2>
+        </div>
+
+        <PostList flatPosts={flatPosts} />
+      </div>
+      <div {...bem('author-item')}>
+        <div className="wrapper">
+          <AuthorItem />
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default BlogIndex
+IndexPage.propTypes = {
+  data: PropTypes.object,
+}
+
+export default IndexPage
 
 export const pageQuery = graphql`
   query IndexPage {
