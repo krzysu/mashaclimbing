337daaa0506e9f8cfa9e1e1bb89eaa8e8eaed952
Kris Urbas <krzysu@interia.pl>
2018-04-05 16:44:38 +0200

    add logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Logo.scss'
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <div className="logo__circle-middle">
+        <div className="logo__circle-inner">
+          <div className="logo__copy">Masha Climbing</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Logo
